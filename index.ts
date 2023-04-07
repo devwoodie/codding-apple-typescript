435a5e2f947f5c4000f67ce8281249895033486c
devwoodie <ehddl453@naver.com>
2023-04-07 15:16:11 +0900

    feat : 타입 변수 지정, readonly

--- index.ts
+++ index.ts
@@ -54,6 +54,7 @@ function handleFunction(x: number | string): void{
 
 handleFunction(20);
 
+// 연습
 function helloName(name?: string){
     if(name){
         console.log(`안녕하세요 ${name}`);
@@ -82,3 +83,94 @@ function handleMarry(pay: number, home: boolean, attr: string): string|void{
     };
 };
 handleMarry(100, true, "상");
+
+// 연습
+function cleanFunction(arr: (number | string)[]){
+    let compClean: number[] = [];
+
+    arr.forEach((num) => {
+        if(typeof num === "string"){
+            compClean.push(Number(num));
+        }else{
+            compClean.push(num);
+        }
+    });
+
+    return compClean;
+};
+console.log(cleanFunction(["1",2,"3"]));
+
+// ---
+let 철수쌤 = { subject : 'math' };
+let 영희쌤 = { subject : ['science', 'english'] };
+let 민수쌤 = { subject : ['science', 'art', 'korean'] };
+function subjectFunction(x:{subject: string | string[]}){
+    if(typeof x.subject === "string"){
+        return x.subject;
+    }else if(Array.isArray(x.subject)){
+        return x.subject[x.subject.length - 1];
+    }else{
+        return "empty";
+    };
+};
+console.log(subjectFunction({subject : ['science', 'english']}));
+
+// 타입 변수에 담아 사용하기
+// type 변수
+type Animal =  string | number | undefined;
+type AnimalType = {name: string, age: number};
+let animal: AnimalType = {name: "kim", age: 234};
+
+// const 변수
+type Girlfriend = {
+    readonly name: string
+    age?: number
+};
+
+const girl: Girlfriend = {
+    name : 'aa'
+};
+// girl.name = "bb"
+
+// type 합치기
+type Name = string;
+type Age = number;
+type Person = Name | Age;
+
+type PositionX = { x: number };
+type PositionY = { y: number };
+type NewType = PositionX & PositionY; // { x: number, y: number }
+
+let position: NewType = {x: 10, y: 20};
+
+// 연습
+type Position1 = { x: number, y: number };
+type Position2 = { y: number };
+type NewType1 = Position1 & Position2; // { x: number, y: number }
+let position1: NewType1 = {x: 10, y: 20};
+
+//---
+type ObjectType = {color ?: string, size: number, readonly position: number[]};
+
+//---
+type Validation = {
+    name: string,
+    phone: number,
+    email?: string
+}
+
+//---
+type Adult = {adult: boolean};
+type NewValidation = Validation & Adult;
+
+let userINfo:NewValidation = {
+    name : 'kim',
+    adult : false,
+    phone : 1234
+}
+
+
+
+
+
+
